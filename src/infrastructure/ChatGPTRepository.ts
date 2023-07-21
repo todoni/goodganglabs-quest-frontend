d3eb2d89e2ef9bf75f958140203a0aa00a2bf864
sohan <sohan@student.42seoul.kr>
2023-07-21 13:49:30 +0900

    Feat: chat GPT repository 구현

--- src/infrastructure/ChatGPTRepository.ts
+++ src/infrastructure/ChatGPTRepository.ts
@@ -1,8 +1,65 @@
+import { AxiosResponse } from "axios";
 import { IChatAIRepository } from "../domain/IChatAIRepository";
+import HttpClient from "../lib/HttpClient";
+
+interface OpenAIResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  usage: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+  choices: [
+    {
+      message: {
+        role: string;
+        content: string;
+      };
+      finish_reason: string;
+      index: number;
+    }
+  ];
+}
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+    "Content-Type": "application/json",
+  },
+};
+
+const http = new HttpClient(process.env.REACT_APP_OPENAI_BASE_URL!, config);
 
 class ChatGPTRepository implements IChatAIRepository {
-  public sendMessage(message: string): Promise<string> {
-    return new Promise(() => {});
+  public async sendMessage(message: string): Promise<string> {
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+    };
+
+    try {
+      const response: AxiosResponse<OpenAIResponse> = await http.post(
+        "chat/completions",
+        data
+      );
+      const answer = response.data.choices[0].message.content;
+      return answer;
+    } catch (error) {
+      console.error("Error sending message:", error);
+      throw new Error("Failed to get a response.");
+    }
   }
 }
 
